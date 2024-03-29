38083e9a059700d467e68f0d3a9ef2520363af99
Guilherme <gui.coelho09@gmail.com>
2024-03-29 07:34:55 -0300

    insert jwt token

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -56,4 +56,18 @@ export class UserService {
 
     return user;
   }
+
+  async findUserByEmail(email: string): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Email not found');
+    }
+
+    return user;
+  }
 }
